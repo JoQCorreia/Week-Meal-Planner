7f949bf6465c03897a584fcd25345c77319d5000
JoQCorreia <joanqcorreia@gmail.com>
2025-09-20 12:30:20 +0100

    Fixed go.sum and dependency import issues

--- Main.go
+++ Main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	// "database/sql"
+	"database/sql"
 	"fmt"
-	// "log"
-	// "modernc.org/sqlite"
-	// "os"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+	"log"
+	"modernc.org/sqlite"
+	"os"
 )
 
 func main() {
 	fmt.Println("Seedling statement (more2com)")
+
+	a := app.New()
+	w := a.NewWindow("Hello World")
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 
 }
